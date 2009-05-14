d744e9ffe15bfeda352bcfe22aab77dc2a012ddd
p.dambrauskas <p.dambrauskas@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-14 20:08:31 +0000

    adresas()
    git-svn-id: http://subversion.assembla.com/svn/mightmedia/v1@90 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- priedai/funkcijos.php
+++ priedai/funkcijos.php
@@ -92,12 +92,12 @@ function utf8_substr($str, $start) {
 }
 // Svetaines adresui gauti
 function adresas() {
-	$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
-	$protocol = substr(strtolower($_SERVER["SERVER_PROTOCOL"]), 0, strpos(strtolower($_SERVER["SERVER_PROTOCOL"]), "/")) . $s;
-	$port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":" . $_SERVER["SERVER_PORT"]);
-	//return $protocol . "://" . $_SERVER['SERVER_NAME'] . $port . $_SERVER['REQUEST_URI'];
-	return $protocol . "://" . $_SERVER['SERVER_NAME'] . $port . '/';
+$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
+$protocol = substr(strtolower($_SERVER["SERVER_PROTOCOL"]), 0, strpos(strtolower($_SERVER["SERVER_PROTOCOL"]), "/")) . $s;
+$port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":" . $_SERVER["SERVER_PORT"]);
+return str_replace('?'.$_SERVER['QUERY_STRING'],'',$protocol . "://" . $_SERVER['SERVER_NAME'] . $port . $_SERVER['REQUEST_URI']);
 }
+
 
 /**
  * Patikrina ar puslapis egzistuoja ir ar vartotojas turi teise ji matyti bei grazinam puslapio ID
