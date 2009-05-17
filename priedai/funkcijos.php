fd757cb9b09e043c0c5c97f0c7510c4c2bd8b0f4
FDisk <FDisk@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-17 10:03:59 +0000

    BUGFIX: adresas()
    git-svn-id: http://subversion.assembla.com/svn/mightmedia/v1@97 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- priedai/funkcijos.php
+++ priedai/funkcijos.php
@@ -92,10 +92,11 @@ function utf8_substr($str, $start) {
 }
 // Svetaines adresui gauti
 function adresas() {
-$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
-$protocol = substr(strtolower($_SERVER["SERVER_PROTOCOL"]), 0, strpos(strtolower($_SERVER["SERVER_PROTOCOL"]), "/")) . $s;
-$port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":" . $_SERVER["SERVER_PORT"]);
-return str_replace('?'.$_SERVER['QUERY_STRING'],'',$protocol . "://" . $_SERVER['SERVER_NAME'] . $port . $_SERVER['REQUEST_URI']);
+	$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
+	$protocol = substr(strtolower($_SERVER["SERVER_PROTOCOL"]), 0, strpos(strtolower($_SERVER["SERVER_PROTOCOL"]), "/")) . $s;
+	$port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":" . $_SERVER["SERVER_PORT"]);
+	 
+	return str_replace('?'.$_SERVER['QUERY_STRING'],'',$protocol . "://" . str_replace('//','/',$_SERVER['SERVER_NAME'] . $port . dirname($_SERVER['REQUEST_URI']))).'/';
 }
 
 
