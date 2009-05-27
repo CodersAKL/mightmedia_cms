7dde32adf9865f12bb61e087dbd074dfb0bb8571
p.dambrauskas <p.dambrauskas@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-27 14:53:14 +0000

    #40
    git-svn-id: http://subversion.assembla.com/svn/mightmedia/v1@145 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- sql-upgrade.sql
+++ sql-upgrade.sql
@@ -1,2 +1,8 @@
 INSERT INTO `nustatymai` (`key`, `val`) VALUES ('keshas', '1');
+ALTER TABLE `grupes` CHANGE `teises` `teises` VARCHAR( 150 ) CHARACTER SET utf8 COLLATE utf8_lithuanian_ci NOT NULL DEFAULT 'N;';
+DELETE FROM `nustatymai` WHERE `key`='Pastas';
+ALTER TABLE `page` CHANGE `teises` `teises` VARCHAR( 150 ) CHARACTER SET utf8 COLLATE utf8_lithuanian_ci NOT NULL DEFAULT 'N;';
 ALTER TABLE `page` ADD `parent` INT( 150 ) NOT NULL DEFAULT '0' AFTER `teises` ;
+UPDATE `page` SET `teises`='N;';
+ALTER TABLE `panel` CHANGE `teises` `teises` VARCHAR( 150 ) CHARACTER SET utf8 COLLATE utf8_lithuanian_ci NOT NULL DEFAULT 'N;';
+UPDATE `panel` SET `teises`='N;';
