bd8f11f9e5dd4fffd7967200d067442a93ed35d3
p.dambrauskas <p.dambrauskas@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-07-10 19:32:57 +0000

    git-svn-id: http://subversion.assembla.com/svn/mightmedia/v1@238 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- puslapiai/pateikti_foto.php
+++ puslapiai/pateikti_foto.php
@@ -264,7 +264,7 @@ VALUES (
 	} else {
 		$kategorijos[] = "Kategorijų nėra";
 	}
-	if (mysql_num_rows($sql) > 0) {
+	if (sizeof($sql) > 0) {
 		$forma = array("Form" => array("enctype" => "multipart/form-data", "action" => '', "method" => "post", "name" => "action"), "Failas:" => array("name" => "failas", "type" => "file", "value" => "", "class"=>"input"), "Pavadinimas:" => array("type" => "text", "value" => '', "name" => "Pavadinimas", "class"=>"input"), "Kategorija:" => array("type" => "select", "value" => $kategorijos, "name" => "cat", "class" => "input", "class"=>"input"), "Aprašymas:" => array("type" => "textarea", "name" => "Aprasymas", "class"=>"input", "rows" => "3", "class" => "input", "value" => ''), //"Paveiksliukas:"=>array("type"=>"text","value"=>(isset($extra['foto']))?input($extra['foto']):'http://',"name"=>"Pav","class"=>"input"),
 			'Pateikti nuotrauką' => array("type" => "submit", "name" => "action", "value" => 'Pateikti nuotrauką'), );
 
