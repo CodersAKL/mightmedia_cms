d4a2ffea81693faf814a82f78ed558bebdcc0556
p.dambrauskas <p.dambrauskas@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-07-25 16:22:16 +0000

    git-svn-id: http://subversion.assembla.com/svn/mightmedia/v1@267 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- paneles/shoutbox.php
+++ paneles/shoutbox.php
@@ -9,10 +9,13 @@
  * @$Revision$
  * @$Date$
  **/
-if (isset($_POST['chat_box']) && !empty($_POST['chat_box']) && !empty($_POST['chat_msg']) && isset($_SESSION['username'])) {
+if (isset($_POST['chat_box']) && !empty($_POST['chat_box']) && !empty($_POST['chat_msg']) ) {
+if (!isset($_COOKIE['komentatorius'])||(isset($_POST['name'])&&$_POST['name']!=$_COOKIE['komentatorius'])) {
+				setcookie("komentatorius", $_POST['name'], time() + 60 * 60 * 24 * 30);
+			}
 	$msg = htmlspecialchars($_POST['chat_msg']);
-	$nick = $_SESSION['username'];
-	$nick_id = $_SESSION['id'];
+	$nick_id = (isset($_SESSION['id']) ? $_SESSION['id'] : 0);
+	$nick = (isset($_SESSION['username']) ? $_SESSION['username'] : (!empty($_POST['name']) ? $_POST['name']:$lang['system']['guest']));
 	mysql_query1("INSERT INTO `" . LENTELES_PRIESAGA . "chat_box` (`nikas`, `msg`, `time`, `niko_id`) VALUES (" . escape($nick) . ", " . escape($msg) . ", NOW(), " . escape($nick_id) . ");");
 	mysql_query1("DELETE FROM `" . LENTELES_PRIESAGA . "chat_box` WHERE time < (NOW() - INTERVAL 31 DAY)");
 	redirect($_SERVER['HTTP_REFERER'],"header");
@@ -20,24 +23,29 @@ if (isset($_POST['chat_box']) && !empty($_POST['chat_box']) && !empty($_POST['ch
 function chatbox() {
 	global $conf, $lang;
 	$extra = '';
-	if (isset($_SESSION['username']) && !empty($_SESSION['username'])) {
+	$name=(isset($_COOKIE['komentatorius'])?$_COOKIE['komentatorius']:$lang['system']['guest']);
+	if ((isset($_SESSION['username']) && !empty($_SESSION['username']))||$conf['kmomentarai_sveciams'] == 1) {
 		$chat_box = '<form name="chat_box" action="" method="post">
+		            '.((isset($conf['kmomentarai_sveciams'])&&$conf['kmomentarai_sveciams'] == 1&&!isset($_SESSION['username']))?'<input type="text" name="name" class="submit" value="' . $name . '"/>':'').'
                 <textarea name="chat_msg" rows="3" cols="10" class="input"></textarea>
                 <br />
                 <input type="submit" name="chat_box" class="submit" value="' . $lang['sb']['send'] . '" />
                 </form>
                 ';
 	} else {
-		$chat_box = '<blockquote><textarea name="chat" rows="3" cols="10" class="input" disabled="disabled">' . $lang['system']['pleaselogin'] . '</textarea>
+		$chat_box = '<textarea name="chat" rows="3" cols="10" class="input" disabled="disabled">' . $lang['system']['pleaselogin'] . '</textarea>
                 <br />
                 <input type="submit" class="submit" name="chat_box" value="' . $lang['sb']['send'] . '" disabled="disabled" />
         ';
 	}
 	$chat_box .= "<hr />";
 	$extras = '';
+	if(isset($conf['kmomentarai_sveciams'])&&$conf['kmomentarai_sveciams'] == 1){
+	$chat = mysql_query1("SELECT * FROM `" . LENTELES_PRIESAGA . "chat_box` ORDER BY `time` DESC LIMIT " . escape((int)$conf['Chat_limit']));
+	}else{
 	$chat = mysql_query1("SELECT SQL_CACHE `" . LENTELES_PRIESAGA . "chat_box`.*,`" . LENTELES_PRIESAGA . "users`.`nick`,`" . LENTELES_PRIESAGA . "users`.`levelis`
 FROM `" . LENTELES_PRIESAGA . "chat_box` Inner Join `" . LENTELES_PRIESAGA . "users` ON `" . LENTELES_PRIESAGA . "chat_box`.`niko_id` = `" . LENTELES_PRIESAGA . "users`.`id`
-ORDER BY `time` DESC LIMIT " . escape((int)$conf['Chat_limit']));
+ORDER BY `time` DESC LIMIT " . escape((int)$conf['Chat_limit']));}
 	$i = 0;
 
 	if (sizeof($chat) > 0) {
@@ -55,7 +63,7 @@ ORDER BY `time` DESC LIMIT " . escape((int)$conf['Chat_limit']));
 			else
 				$tr = "";
 			$chat_box .= '<div class="tr' . $tr . '">	
-      ' . user($row['nick'], $row['niko_id'], $row['levelis']) . $extras . ' <br />
+      ' . user($row['nikas'], $row['niko_id']) . $extras . ' <br />
         ' . smile(bbchat(wrap($row['msg'], 18))) . '<br /></div>
       ';
 		}
