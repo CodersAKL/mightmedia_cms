a5caf78e3bdc0b58a4155a3df259da634eb12769
FDisk <FDisk@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-08-03 19:50:55 +0000

    Ai niekas
    git-svn-id: http://subversion.assembla.com/svn/mightmedia/v1@280 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- paneles/shoutbox.php
+++ paneles/shoutbox.php
@@ -9,70 +9,83 @@
  * @$Revision$
  * @$Date$
  **/
-if (isset($_POST['chat_box']) && !empty($_POST['chat_box']) && !empty($_POST['chat_msg']) ) {
-if (!isset($_COOKIE['komentatorius'])||(isset($_POST['name'])&&$_POST['name']!=$_COOKIE['komentatorius'])) {
-				setcookie("komentatorius", $_POST['name'], time() + 60 * 60 * 24 * 30);
-			}
-	$msg = htmlspecialchars($_POST['chat_msg']);
-	$nick_id = (isset($_SESSION['id']) ? $_SESSION['id'] : 0);
-	$nick = (isset($_SESSION['username']) ? $_SESSION['username'] : (!empty($_POST['name']) ? $_POST['name']:$lang['system']['guest']));
-	mysql_query1("INSERT INTO `" . LENTELES_PRIESAGA . "chat_box` (`nikas`, `msg`, `time`, `niko_id`) VALUES (" . escape($nick) . ", " . escape($msg) . ", NOW(), " . escape($nick_id) . ");");
-	mysql_query1("DELETE FROM `" . LENTELES_PRIESAGA . "chat_box` WHERE time < (NOW() - INTERVAL 31 DAY)");
+
+if(isset($_POST['chat_box']) && !empty($_POST['chat_box']) && !empty($_POST['chat_msg'])) {
+	if(!isset($_COOKIE['komentatorius']) || (isset($_POST['name']) && $_POST['name'] != $_COOKIE['komentatorius'])) {
+		setcookie("komentatorius",$_POST['name'],time() + 60 * 60 * 24 * 30);
+	}
+	$msg		= htmlspecialchars($_POST['chat_msg']);
+	$nick_id	= (isset($_SESSION['id']) ? $_SESSION['id'] : 0);
+	$nick		= (isset($_SESSION['username']) ? $_SESSION['username'] : (!empty($_POST['name']) ? $_POST['name'] : $lang['system']['guest']));
+
+	mysql_query1("INSERT INTO `".LENTELES_PRIESAGA."chat_box`
+		(`nikas`, `msg`, `time`, `niko_id`)
+		VALUES (".escape($nick).", ".escape($msg).", NOW(), ".escape($nick_id).");"
+	);
+	mysql_query1("DELETE FROM `".LENTELES_PRIESAGA."chat_box` WHERE time < (NOW() - INTERVAL 31 DAY)");
 	redirect($_SERVER['HTTP_REFERER'],"header");
 }
 function chatbox() {
-	global $conf, $lang;
+	global $conf,$lang;
 	$extra = '';
-	$name=(isset($_COOKIE['komentatorius'])?$_COOKIE['komentatorius']:$lang['system']['guest']);
-	if ((isset($_SESSION['username']) && !empty($_SESSION['username']))||$conf['kmomentarai_sveciams'] == 1) {
+	$name = (isset($_COOKIE['komentatorius']) ? $_COOKIE['komentatorius'] : $lang['system']['guest']);
+	if((isset($_SESSION['username']) && !empty($_SESSION['username'])) || $conf['kmomentarai_sveciams'] == 1) {
 		$chat_box = '<form name="chat_box" action="" method="post">
-		            '.((isset($conf['kmomentarai_sveciams'])&&$conf['kmomentarai_sveciams'] == 1&&!isset($_SESSION['username']))?'<input type="text" name="name" class="submit" value="' . $name . '"/>':'').'
+		            '.((isset($conf['kmomentarai_sveciams']) && $conf['kmomentarai_sveciams'] == 1 && !isset($_SESSION['username'])) ? '<input type="text" name="name" class="submit" value="'.$name.'"/>' : '').'
                 <textarea name="chat_msg" rows="3" cols="10" class="input"></textarea>
                 <br />
-                <input type="submit" name="chat_box" class="submit" value="' . $lang['sb']['send'] . '" />
+                <input type="submit" name="chat_box" class="submit" value="'.$lang['sb']['send'].'" />
                 </form>
                 ';
 	} else {
-		$chat_box = '<textarea name="chat" rows="3" cols="10" class="input" disabled="disabled">' . $lang['system']['pleaselogin'] . '</textarea>
+		$chat_box = '<textarea name="chat" rows="3" cols="10" class="input" disabled="disabled">'.$lang['system']['pleaselogin'].'</textarea>
                 <br />
-                <input type="submit" class="submit" name="chat_box" value="' . $lang['sb']['send'] . '" disabled="disabled" />
+                <input type="submit" class="submit" name="chat_box" value="'.$lang['sb']['send'].'" disabled="disabled" />
         ';
 	}
 	$chat_box .= "<hr />";
 	$extras = '';
-	if(isset($conf['kmomentarai_sveciams'])&&$conf['kmomentarai_sveciams'] == 1){
-	$chat = mysql_query1("SELECT * FROM `" . LENTELES_PRIESAGA . "chat_box` ORDER BY `time` DESC LIMIT " . escape((int)$conf['Chat_limit']));
-	}else{
-	$chat = mysql_query1("SELECT SQL_CACHE `" . LENTELES_PRIESAGA . "chat_box`.*,`" . LENTELES_PRIESAGA . "users`.`nick`,`" . LENTELES_PRIESAGA . "users`.`levelis`
-FROM `" . LENTELES_PRIESAGA . "chat_box` Inner Join `" . LENTELES_PRIESAGA . "users` ON `" . LENTELES_PRIESAGA . "chat_box`.`niko_id` = `" . LENTELES_PRIESAGA . "users`.`id`
-ORDER BY `time` DESC LIMIT " . escape((int)$conf['Chat_limit']));}
+	if(isset($conf['kmomentarai_sveciams']) && $conf['kmomentarai_sveciams'] == 1) {
+		$chat = mysql_query1("SELECT * FROM `".LENTELES_PRIESAGA."chat_box` ORDER BY `time` DESC LIMIT ".escape((int) $conf['Chat_limit']));
+	} else {
+		$chat = mysql_query1("SELECT SQL_CACHE
+			`".LENTELES_PRIESAGA."chat_box`.*,
+			`".LENTELES_PRIESAGA."users`.`nick`,
+			`".LENTELES_PRIESAGA."users`.`levelis`
+			FROM `".LENTELES_PRIESAGA."chat_box`
+			Inner Join `".LENTELES_PRIESAGA."users`
+			ON `".LENTELES_PRIESAGA."chat_box`.`niko_id` = `".LENTELES_PRIESAGA."users`.`id`
+			ORDER BY `time` DESC LIMIT ".escape((int) $conf['Chat_limit'])
+		);
+	}
 	$i = 0;
 
-	if (sizeof($chat) > 0) {
-		foreach ($chat as $row) {
+	if(sizeof($chat) > 0) {
+		foreach($chat as $row) {
 			$i++;
-			if (isset($_SESSION['level']) && ($_SESSION['level'] == 1 || (isset($_SESSION['mod']) && strlen($_SESSION['mod']) > 1)) && isset($conf['puslapiai']['deze.php']['id'])) {
+			if(isset($_SESSION['level']) && ($_SESSION['level'] == 1 || (isset($_SESSION['mod']) && strlen($_SESSION['mod']) > 1)) && isset($conf['puslapiai']['deze.php']['id'])) {
 				$extras = "
-        <a title='{$lang['admin']['delete']}' href='?id," . $conf['puslapiai']['deze.php']['id'] . ";d," . $row['id'] . "'><img src='images/icons/control_delete_small.png' alt='[d]' class='middle' border='0' /></a>
-        <a title='{$lang['admin']['edit']}' href='?id," . $conf['puslapiai']['deze.php']['id'] . ";r," . $row['id'] . "'><img src='images/icons/brightness_small_low.png' alt='[r]' class='middle' border='0' /></a>
-        
+        <a title='{$lang['admin']['delete']}' href='?id,".$conf['puslapiai']['deze.php']['id'].";d,".$row['id']."'><img src='images/icons/control_delete_small.png' alt='[d]' class='middle' border='0' /></a>
+        <a title='{$lang['admin']['edit']}' href='?id,".$conf['puslapiai']['deze.php']['id'].";r,".$row['id']."'><img src='images/icons/brightness_small_low.png' alt='[r]' class='middle' border='0' /></a>
+
       ";
 			}
-			if (is_int($i / 2))
+			if(is_int($i / 2)) {
 				$tr = "2";
-			else
+			} else {
 				$tr = "";
-			$chat_box .= '<div class="tr' . $tr . '">	
-      ' . user($row['nikas'], $row['niko_id']) . $extras . ' <br />
-        ' . smile(bbchat(wrap($row['msg'], 18))) . '<br /></div>
+			}
+			$chat_box .= '<div class="tr'.$tr.'">
+      '.user($row['nikas'],$row['niko_id']).$extras.' <br />
+        '.smile(bbchat(wrap($row['msg'],18))).'<br /></div>
       ';
 		}
 	} else {
 		$chat_box .= '';
 	}
 
-	if (puslapis('deze.php')) {
-		$chat_box .= "<a href='?id," . $conf['puslapiai']['deze.php']['id'] . "' >{$lang['sb']['archive']}</a>";
+	if(puslapis('deze.php')) {
+		$chat_box .= "<a href='?id,".$conf['puslapiai']['deze.php']['id']."' >{$lang['sb']['archive']}</a>";
 	}
 	return $chat_box;
 }
