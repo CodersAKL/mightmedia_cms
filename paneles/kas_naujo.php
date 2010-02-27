e9fb602d319f0f4bead51baf6529ecf8bcc8d9fa
FDisk <FDisk@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-02-27 16:06:18 +0000

    ups
    git-svn-id: http://subversion.assembla.com/svn/mightmedia/v1@384 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- paneles/kas_naujo.php
+++ paneles/kas_naujo.php
@@ -71,7 +71,7 @@ if (isset($_SESSION['lankesi']) && $_SESSION['lankesi'] > 0) {
 		if (sizeof($q) > 0) {
 			$text .= "<b>{$lang['new']['urls']}:</b><br/>";
 			foreach ($q as $row) {
-				$text .= "<img src=\"images/icons/brightness_small_low.png\" alt=\"o\" class=\"middle\" border=\"0\"/> <a href='?id," . $conf['puslapiai']['nuorodos.php']['id'] . ";k,". $row['cat'] .";w," . $row['id'] . "'>" . trimlink(input($row['pavadinimas']), 20) . "</a><br />\n";
+				$text .= "<img src=\"images/icons/brightness_small_low.png\" alt=\"o\" class=\"middle\" border=\"0\"/> <a href='?id," . $conf['puslapiai']['nuorodos.php']['id'] . ";k,". $row['cat'] .";w," . $row['id'] . "' target='_blank' rel='nofollow'>" . trimlink(input($row['pavadinimas']), 20) . "</a><br />\n";
 			}
 		}
 	}
