41531502004fe8cb6a931126ecbc41e87f2d1fb4
FDisk <FDisk@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-03-13 14:47:33 +0000

    Galerija bugfix
    git-svn-id: http://subversion.assembla.com/svn/mightmedia/v1@434 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- dievai/galerija.php
+++ dievai/galerija.php
@@ -69,9 +69,9 @@ if (((isset($_POST['action']) && $_POST['action'] == $lang['admin']['delete']  &
 	$sql = mysql_query1("SELECT `file` FROM `" . LENTELES_PRIESAGA . "galerija` WHERE `ID` = " . escape($trinti) . " LIMIT 1");
 
 	if (isset($row['file']) && !empty($row['file'])) {
-		@unlink("images/galerija/" . $row['file']);
-		@unlink("images/galerija/mini/" . $row['file']);
-		@unlink("images/galerija/originalai/" . $row['file']);
+		@unlink(ROOT."images/galerija/" . $row['file']);
+		@unlink(ROOT."images/galerija/mini/" . $row['file']);
+		@unlink(ROOT."images/galerija/originalai/" . $row['file']);
 	}
 	mysql_query1("DELETE FROM `" . LENTELES_PRIESAGA . "galerija` WHERE id=" . escape($trinti) . " LIMIT 1");
 
