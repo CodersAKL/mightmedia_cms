62216cf6e566e6d92dc2ee58015f032c0b1fb6bf
p.dambrauskas <p.dambrauskas@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-05-21 16:36:21 +0000

    git-svn-id: http://subversion.assembla.com/svn/mightmedia/v1@591 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- puslapiai/blsavimo_archyvas.php
+++ puslapiai/blsavimo_archyvas.php
@@ -1,18 +1,21 @@
 <?php
 if(!isset($_GET['m'])){
-  $limit=20;
-  $viso=kiek("poll_questions", "WHERE `shown`='1' AND `lang` = ".escape(lang())."");
-  $p=(isset($_GET['p'])?$_GET['p']:0);
-  $quests = mysql_query1("SELECT *, `id` as `qid`, (SELECT count(id) FROM `".LENTELES_PRIESAGA."poll_votes` WHERE `question_id`=`qid`) as `votes` FROM `".LENTELES_PRIESAGA."poll_questions` WHERE `shown`='1' AND `lang` = ".escape(lang())." ORDER BY `id` DESC LIMIT $p , $limit");
-  $text = '<ul>';
-  foreach ($quests as $row){
-    $text .= "<li><a href =\"".url("?id,{$_GET['id']};m,{$row['id']}")."\">".input($row['question'])."</a> ({$row['votes']})</li>";
-  }
-  $text .= '</ul>';
-  lentele($lang['poll']['archive'], $text);
-  if ($viso > $limit) {
-    lentele($lang['system']['pages'], puslapiai($p, $limit, $viso, 10));
-  }
+  $limit = 20;
+  $viso = kiek("poll_questions", "WHERE `shown`='1' AND `lang` = ".escape(lang())."");
+  if($viso > 0){
+    $p=(isset($_GET['p'])?$_GET['p']:0);
+    $quests = mysql_query1("SELECT *, `id` as `qid`, (SELECT count(id) FROM `".LENTELES_PRIESAGA."poll_votes` WHERE `question_id`=`qid`) as `votes` FROM `".LENTELES_PRIESAGA."poll_questions` WHERE `shown`='1' AND `lang` = ".escape(lang())." ORDER BY `id` DESC LIMIT $p , $limit");
+    $text = '<ul>';
+    foreach ($quests as $row){
+      $text .= "<li><a href =\"".url("?id,{$_GET['id']};m,{$row['id']}")."\">".input($row['question'])."</a> ({$row['votes']})</li>";
+    }
+    $text .= '</ul>';
+    lentele($lang['poll']['archive'], $text);
+    if ($viso > $limit) {
+      lentele($lang['system']['pages'], puslapiai($p, $limit, $viso, 10));
+    }
+  } else 
+      klaida($lang['system']['warning'], $lang['system']['no_content']);
 } else {
     $quest = mysql_query1("SELECT * FROM `".LENTELES_PRIESAGA."poll_questions` WHERE `shown`='1' AND `lang` = ".escape(lang())." AND `id`=".escape($_GET['m'])." ORDER BY `id` DESC LIMIT 1");
     if(isset($quest['question'])){
