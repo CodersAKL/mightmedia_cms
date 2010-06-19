76c1f8ce93339ce8ca648bcb218071cf62e19434
p.dambrauskas <p.dambrauskas@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-06-19 15:21:54 +0000

    git-svn-id: http://subversion.assembla.com/svn/mightmedia/v1@703 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- priedai/funkcijos.php
+++ priedai/funkcijos.php
@@ -339,7 +339,7 @@ function cleanurl($url) {
  * @return array
  */
 unset($sql, $row);
-if($_SERVER['PHP_SELF'] != 'upgrade.php') {
+if(basename($_SERVER['PHP_SELF']) != 'upgrade.php') {
   $sql = mysql_query1("SELECT * FROM `" . LENTELES_PRIESAGA . "grupes` WHERE `kieno` = 'vartotojai' AND `lang`=".escape(lang())." ORDER BY `id` DESC");
 
   if (sizeof($sql) > 0) {
