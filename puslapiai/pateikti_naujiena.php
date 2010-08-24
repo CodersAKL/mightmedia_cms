94929dcf308a7b935a0ff7d2cf99762564303ad6
p.dambrauskas <p.dambrauskas@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-08-24 09:29:47 +0000

    git-svn-id: http://subversion.assembla.com/svn/mightmedia/v1@727 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- puslapiai/pateikti_naujiena.php
+++ puslapiai/pateikti_naujiena.php
@@ -24,29 +24,14 @@ if (sizeof($sql) > 0) {
 if (isset($_POST['action']) && $_POST['action'] == 'Pateikti') {
 	//print_r($_POST);
 	if (isset($_POST['naujiena']) && isset($_POST['pav'])) {
-		
-
-		$naujiena = $_POST['naujiena'];
-		$komentaras = (isset($_POST['kom']) && $_POST['kom'] == 'taip' ? 'taip' : 'ne');
-		$pavadinimas = input(strip_tags($_POST['pav']));
-		$kategorija = (int)$_POST['kategorija'];
-		$autorius = (isset($_SESSION['username']) ? $_SESSION['username'] : 'Svečias');
-
-		if (empty($naujiena) || empty($pavadinimas)) {
-			$error = "Nepilnai užpildyti laukeliai";
-		}
-		if (!isset($error)) {
-			$result = mysql_query1("
-				INSERT INTO `" . LENTELES_PRIESAGA . "naujienos` (pavadinimas, naujiena, daugiau, data, autorius, kom, kategorija, lang)
-				VALUES (" . escape($pavadinimas) . ", " . escape($naujiena) . ", '',  '" . time() . "', '" . $autorius . "', " . escape($komentaras) . ", " . escape($kategorija) . ", ".escape(lang()).")");
-			if ($result) {
-				msg("Informacija", "Naujiena sėkmingai pateikta administracijos peržiūrai");
-			} else {
-				klaida("Klaida", "Naujiena nepatalpinta. Klaida:<br><b>" . mysql_error() . "</b>");
-			}
-		} else {
-			klaida("Klaida", $error);
-		}
+	$naujiena = explode('===page===', $_POST['naujiena']);
+	$izanga = $naujiena[0];
+	$placiau = empty($naujiena[1]) ? '' : $naujiena[1];
+	$komentaras = (isset($_POST['kom']) ? $_POST['kom'] : 'taip');
+	$pavadinimas = strip_tags($_POST['pav']);
+	$kategorija = (int) $_POST['kategorija'];
+	$result = mysql_query1("INSERT INTO `" . LENTELES_PRIESAGA . "naujienos` (pavadinimas, naujiena, daugiau, data, autorius, kom, kategorija, rodoma, lang) VALUES (" . escape($pavadinimas) . ", " . escape($izanga) . ", " . escape($placiau) . ",  '" . time() . "', '" . (isset($_SESSION['username'])?$_SESSION['username']:'Svečias'). "', " . escape($komentaras) . ", " . escape($kategorija) . ", 'NE', ".escape(lang()).")");
+    msg('Atlikta', 'Naujiena pateikta administracijos peržiūrai.');
 		redirect(url("?id," . $_GET['id']), "meta");
 
 	} else {
@@ -61,7 +46,7 @@ if ($i == 1) {
 		"Pavadinimas:" => array("type" => "text", "value" => '', "name" => "pav", "class"=>"input"),
 		"Komentarai:" => array("type" => "select", "value" => array('taip' => 'TAIP', 'ne' => 'NE'), "name" => "kom", "class" => "input", "class"=>"input"),
 		"Kategorija:" => array("type" => "select", "value" => $kategorijos, "name" => "kategorija", "class" => "input", "class"=>"input"),
-		"Naujiena:" => array("type" => "string", "value" => editorius('tiny_mce', 'standartinis', array('naujiena' => 'Glaustai', 'placiau' => 'Plačiau'))),
+		"Naujiena:" => array("type" => "string", "value" => editorius('tiny_mce', 'standartinis', 'naujiena')),
 		'Pateikti' => array("type" => "submit", "name" => "action", "value" => 'Pateikti')
 	);
 	lentele('Naujienos rašymas', $bla->form($naujiena));
