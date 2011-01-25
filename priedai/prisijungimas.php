7db892d662ace2c761b959d9402453382d6388b3
FDisk <FDisk@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2011-01-25 01:11:07 +0000

    Paskutinio apsilankymo bugas
    git-svn-id: http://subversion.assembla.com/svn/mightmedia/v1@785 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- priedai/prisijungimas.php
+++ priedai/prisijungimas.php
@@ -8,28 +8,29 @@
  * @license GNU General Public License v2
  * @$Revision$
  * @$Date$
- **/
- 
+ * */
 //FDISK, nenaudok session_destroy(); ir session_unset(); šiam faile, nes jie tuo pačiu ir forumo sausainius išvalo
 //Auto Atjungimas nuo sistemos (neveikė)
 //svecio lygis = 0
-if(!isset($_SESSION['level'])){
-    $_SESSION['level'] = 0;
-		$_SESSION['mod'] = serialize(array());
+if (!isset($_SESSION['level'])) {
+	$_SESSION['level'] = 0;
+	$_SESSION['mod'] = serialize(array());
 }
+
 function login($array) {
-  	$_SESSION['username'] = $array['nick'];
-		$_SESSION['password'] = $array['pass'];
-		$_SESSION['id'] = (int)$array['id'];
-		$_SESSION['lankesi'] = $array['login_before'];
-		$_SESSION['level'] = $array['levelis'];
-		$_SESSION['mod'] = $array['mod'];
+	$_SESSION['username'] = $array['nick'];
+	$_SESSION['password'] = $array['pass'];
+	$_SESSION['id'] = (int) $array['id'];
+	$_SESSION['lankesi'] = $array['login_before'];
+	$_SESSION['level'] = $array['levelis'];
+	$_SESSION['mod'] = $array['mod'];
 }
-function logout(){
-    unset($_SESSION['username'],$_SESSION['password'],$_SESSION['id'],$_SESSION['level'],$_SESSION['mod']); // Isvalom sesija
-		$_SESSION['level'] = 0;
-		$_SESSION['mod'] = serialize(array());
-		setcookie("user", "", time() - 3600); // Sunaikinam sesija
+
+function logout() {
+	unset($_SESSION['username'], $_SESSION['password'], $_SESSION['id'], $_SESSION['level'], $_SESSION['mod']); // Isvalom sesija
+	$_SESSION['level'] = 0;
+	$_SESSION['mod'] = serialize(array());
+	setcookie("user", "", time() - 3600); // Sunaikinam sesija
 }
 
 //tikrinam sesija
@@ -46,9 +47,8 @@ if (isset($_SESSION['username']) && isset($_SESSION['password'])) {
 	if (isnum($user_id['0'])) {
 		$user_pass = $user_id['1'];
 		$user_id = $user_id['0'];
-
 	}
-	$linformacija2 = mysql_query1("SELECT `id`, `levelis`,`pass`,`nick`,`login_data`,`login_before`, (SELECT `mod` FROM `" . LENTELES_PRIESAGA . "grupes` WHERE `id`=levelis) as `mod` FROM `" . LENTELES_PRIESAGA . "users` WHERE `id`=" . escape((int)$user_id) . " LIMIT 1");
+	$linformacija2 = mysql_query1("SELECT `id`, `levelis`,`pass`,`nick`,`login_data`,`login_before`, (SELECT `mod` FROM `" . LENTELES_PRIESAGA . "grupes` WHERE `id`=levelis) as `mod` FROM `" . LENTELES_PRIESAGA . "users` WHERE `id`=" . escape((int) $user_id) . " LIMIT 1");
 	if (!empty($linformacija2['levelis']) && isset($user_pass) && koduoju($slaptas . getip() . $linformacija2['pass']) === $user_pass) {
 		$result = mysql_query1("UPDATE `" . LENTELES_PRIESAGA . "users` SET `login_before`=login_data, `login_data` = '" . time() . "', `ip` = INET_ATON(" . escape(getip()) . ") WHERE `id` ='" . escape($user_id) . "' LIMIT 1");
 		login($linformacija2);
@@ -65,7 +65,7 @@ if (isset($_POST['action']) && $_POST['action'] == 'prisijungimas') {
 	if (!isset($_SESSION['login_error']) || $_SESSION['login_error'] <= 4) {
 		$strUsername = $_POST['vartotojas']; // Vartotojo vardas
 		$strPassword = koduoju($_POST['slaptazodis']); // Slaptazodis
-    $linformacija3 = mysql_query1("SELECT `id`,`levelis`,`pass`,`nick`,`login_data`,`login_before`,(SELECT `mod` FROM `" . LENTELES_PRIESAGA . "grupes` WHERE `id`=levelis) as `mod` FROM `" . LENTELES_PRIESAGA . "users` WHERE hex(nick)=hex(" . escape($strUsername) . ") AND password(pass)=password('" . $strPassword . "') LIMIT 1");
+		$linformacija3 = mysql_query1("SELECT `id`,`levelis`,`pass`,`nick`,`login_data`,`login_before`,(SELECT `mod` FROM `" . LENTELES_PRIESAGA . "grupes` WHERE `id`=levelis) as `mod` FROM `" . LENTELES_PRIESAGA . "users` WHERE hex(nick)=hex(" . escape($strUsername) . ") AND password(pass)=password('" . $strPassword . "') LIMIT 1");
 		if (!empty($linformacija3) && $strPassword === $linformacija3['pass']) {
 			login($linformacija3);
 			mysql_query1("UPDATE `" . LENTELES_PRIESAGA . "users` SET `login_before`=login_data, `login_data` = '" . time() . "', `ip` = INET_ATON(" . escape(getip()) . ") WHERE `id` ='" . $linformacija3['id'] . "' LIMIT 1");
@@ -73,9 +73,8 @@ if (isset($_POST['action']) && $_POST['action'] == 'prisijungimas') {
 				setcookie("user", $_SESSION['id'] . "." . koduoju($slaptas . getip() . $_SESSION['password']), time() + 60 * 60 * 24 * 30);
 			}
 			header("Location: " . (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : adresas()));
-
 		} else {
-			mysql_query1("INSERT INTO `" . LENTELES_PRIESAGA . "logai` (`action` ,`time` ,`ip`) VALUES (" . escape("{$lang['user']['wrong']}: User: " . $strUsername . " Pass: " . str_repeat('*',strlen($_POST['slaptazodis']))) . ",'" . time() . "',INET_ATON(" . escape(getip()) . "));");
+			mysql_query1("INSERT INTO `" . LENTELES_PRIESAGA . "logai` (`action` ,`time` ,`ip`) VALUES (" . escape("{$lang['user']['wrong']}: User: " . $strUsername . " Pass: " . str_repeat('*', strlen($_POST['slaptazodis']))) . ",'" . time() . "',INET_ATON(" . escape(getip()) . "));");
 			$strError = $lang['user']['wrong'];
 			// + bandymas
 			isset($_SESSION['login_error']) ? $_SESSION['login_error']++ : $_SESSION['login_error'] = 1;
@@ -83,13 +82,11 @@ if (isset($_POST['action']) && $_POST['action'] == 'prisijungimas') {
 			$_SESSION['timeout_idle'] = time() + ini_get('session.cache_expire');
 		}
 		unset($linfo, $strUsername, $strPassword);
-
 	} else {
 		$strError = "{$lang['user']['cantlogin']}<span id='sekundes'>" . ($_SESSION['timeout_idle'] - time()) . "</span></b><script>startCount();</script>s. ";
 		//jeigu baigesi laikas
-    if($_SESSION['timeout_idle'] - time() <= 0)
-       unset($_SESSION['timeout_idle'], $_SESSION['login_error']);
-
+		if ($_SESSION['timeout_idle'] - time() <= 0)
+			unset($_SESSION['timeout_idle'], $_SESSION['login_error']);
 	}
 }
 //jei paspaude atsijungti
@@ -100,5 +97,4 @@ if (isset($_GET['id']) && !empty($_GET['id']) && $_GET['id'] == $lang['user']['l
 	header("Location: " . (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : adresas()));
 }
 //print_r($_SESSION);
-
 ?> 
