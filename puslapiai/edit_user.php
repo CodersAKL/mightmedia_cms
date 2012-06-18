8115ed2b83ad6b81e144d48d48a7c648480fc924
p.dambrauskas <p.dambrauskas@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2012-06-18 18:04:14 +0000

    pravalymas
    git-svn-id: http://subversion.assembla.com/svn/mightmedia/v1@818 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- puslapiai/edit_user.php
+++ puslapiai/edit_user.php
@@ -12,17 +12,19 @@ if (!defined("OK") || !isset($_SESSION['username'])) {
 	header("Location: " . url("?id,{$conf['puslapiai'][$conf['pirminis'] . '.php']['id']}"));
 	exit;
 }
-
+include_once ("priedai/class.php");
 $mid = isset($url['m']) ? $url['m'] : 0;
 $id = $url['id'];
 // ############ Apdorojomi duomenys kurie buvo pateikti is tam tikros redagavimo lenteles #####################
 // ######### Slaptazodzio keitimas #############
 if (isset($_POST['old_pass']) && count($_POST['old_pass']) > 0 && count($_POST['new_pass']) > 0 && count($_POST['new_pass2']) > 0) {
 	$old_pass = koduoju($_POST['old_pass']);
-	$sql = count(mysql_query1("SELECT * FROM `" . LENTELES_PRIESAGA . "users` WHERE nick=" . escape($_SESSION['username']) . " AND pass=" . escape($old_pass)));
-	if ($sql != 0) {
+	$user = mysql_query1("SELECT `nick` FROM `" . LENTELES_PRIESAGA . "users` WHERE `nick`=" . escape($_SESSION['username']) . " AND pass=" . escape($old_pass) . " LIMIT 1");
+    //ar teisingas senas slaptzodis
+	if (isset($user['nick'])) {
 		$new_pass = koduoju($_POST['new_pass']);
 		$new_pass2 = koduoju($_POST['new_pass2']);
+        //ar sutampa ivesti nauji slaptazodziai
 		if ($new_pass == $new_pass2) {
 			mysql_query1("UPDATE `" . LENTELES_PRIESAGA . "users` SET pass=" . escape($new_pass) . " WHERE nick=" . escape($_SESSION['username']));
 			msg($lang['system']['done'], $lang['user']['edit_updated']);
@@ -32,7 +34,7 @@ if (isset($_POST['old_pass']) && count($_POST['old_pass']) > 0 && count($_POST['
 	} else {
 		klaida($lang['system']['error'], $lang['user']['edit_badpass']);
 	}
-	unset($old_pass, $sql, $new_pass, $new_pass2);
+	unset($old_pass, $user, $new_pass, $new_pass2);
 }
 // ################# kontaktu keitimas ######################
 if (isset($_POST['action']) && $_POST['action'] == 'contacts_change') {
@@ -95,19 +97,18 @@ $text = "
 ";
 lentele($lang['user']['edit_settings'], $text);
 // ######################### Jei pasirinktas vienas is pasiulytu MENIU ####################
-include_once ("priedai/class.php");
-if (isset($mid) && isnum($mid)) {
+
 	// Pakeisti slaptazodi
-	if ($mid == 1) {
-		$form = array("Form" => array("action" => "", "method" => "post", "enctype" => "", "id" => "", "class" => "", "name" => "change_password"), "{$lang['user']['edit_pass']}:" => array("type" => "password", "value" => "", "name" => "old_pass"), "{$lang['user']['edit_newpass']}:" => array("type" => "password", "value" => "", "name" => "new_pass"), "{$lang['user']['edit_confirmnewpass']}:" => array("type" => "password", "value" => "", "name" => "new_pass2"), "" => array("type" => "hidden", "name" => "action", "value" => "pass_change"), "" => array("type" => "submit", "value" => "{$lang['user']['edit_update']}"));
-		$bla = new forma();
-		lentele($lang['user']['edit_pass'], $bla->form($form));
-	}
-	// Pakeisti kontaktinius duomenis
-	elseif ($mid == 2) {
+if ($mid == 1) {
+		$form_array = array("Form" => array("action" => "", "method" => "post", "enctype" => "", "id" => "", "class" => "", "name" => "change_password"), "{$lang['user']['edit_pass']}:" => array("type" => "password", "value" => "", "name" => "old_pass"), "{$lang['user']['edit_newpass']}:" => array("type" => "password", "value" => "", "name" => "new_pass"), "{$lang['user']['edit_confirmnewpass']}:" => array("type" => "password", "value" => "", "name" => "new_pass2"), "" => array("type" => "hidden", "name" => "action", "value" => "pass_change"), "" => array("type" => "submit", "value" => "{$lang['user']['edit_update']}"));
+		$form = new forma();
+		lentele($lang['user']['edit_pass'], $bla->form($form_array));
+}
+// Pakeisti kontaktinius duomenis
+elseif ($mid == 2) {
 		$info = mysql_query1("SELECT * FROM `" . LENTELES_PRIESAGA . "users` WHERE `nick`=" . escape($_SESSION['username']) . "LIMIT 1");
 
-		$form = array(
+		$form_array = array(
 			"Form" => array("action" => url("?id," . $conf['puslapiai'][basename(__file__)]['id'] . ";m," . $_GET['m']), "method" => "post", "enctype" => "", "id" => "", "extra" => "onSubmit=\"return checkMail('change_contacts','email')\"", "name" => "change_contacts"),
 			"ICQ:" => array("type" => "text", "value" => input($info['icq']), "name" => "icq", "class" => "input"),
 			"MSN:" => array("type" => "text", "value" => input($info['msn']), "name" => "msn", "class" => "input"),
@@ -117,13 +118,13 @@ if (isset($mid) && isnum($mid)) {
 			"{$lang['user']['edit_web']}:" => array("type" => "text", "value" => input($info['url']), "name" => "url", "class" => "input"),
 			"{$lang['user']['edit_email']}:" => array("type" => "text", "value" => input($info['email']), "name" => "email", "class" => "input"),
 			"\r\r\r" => array("type" => "hidden", "name" => "action", "value" => "contacts_change"),
-			"" => array("type" => "submit", "value" => "{$lang['user']['edit_update']}")
+			"" => array("type" => "submit", "value" => $lang['user']['edit_update'])
 		);
-		$bla = new forma();
-		lentele($lang['user']['edit_contacts'], $bla->form($form));
-	}
-	// Pakeisti sali, miesta
-	elseif ($mid == 3) {
+		$form = new forma();
+		lentele($lang['user']['edit_contacts'], $bla->form($form_array));
+}
+// Pakeisti sali, miesta
+elseif ($mid == 3) {
 		$info = mysql_query1("SELECT `salis`, `miestas` FROM `" . LENTELES_PRIESAGA . "users` WHERE `nick`=" . escape($_SESSION['username']) . " LIMIT 1");
 
 		$sql = mysql_query1("SELECT * FROM `" . LENTELES_PRIESAGA . "salis`");
@@ -132,15 +133,14 @@ if (isset($mid) && isnum($mid)) {
 			$salis[$row['iso']] = $row['printable_name'];
 		}
 
-		$forma = array("Form" => array("action" => url("?id," . $conf['puslapiai'][basename(__file__)]['id'] . ";m," . $_GET['m']), "method" => "post", "name" => "change_country"), "{$lang['user']['edit_country']}:" => array("type" => "select", "value" => $salis, "name" => "salis", "selected" => input($info['salis'])), "{$lang['user']['edit_city']}:" => array("type" => "text", "value" => input($info['miestas']), "name" => "miestas"), " \r " => array("type" => "hidden", "name" => "action", "value" => "country_change"), "" => array("type" => "submit", "value" => "{$lang['user']['edit_update']}"));
+		$form_array = array("Form" => array("action" => url("?id," . $conf['puslapiai'][basename(__file__)]['id'] . ";m," . $_GET['m']), "method" => "post", "name" => "change_country"), "{$lang['user']['edit_country']}:" => array("type" => "select", "value" => $salis, "name" => "salis", "selected" => input($info['salis'])), "{$lang['user']['edit_city']}:" => array("type" => "text", "value" => input($info['miestas']), "name" => "miestas"), " \r " => array("type" => "hidden", "name" => "action", "value" => "country_change"), "" => array("type" => "submit", "value" => $lang['user']['edit_update']));
 
-		$bla = new forma();
-		lentele($lang['user']['edit_locality'], $bla->form($forma));
-	}
+		$form = new forma();
+		lentele($lang['user']['edit_locality'], $bla->form($form_array));
+}
 
-	// Avataro keitimas
-	//Žaidime mano šito nereikės
-	elseif ($mid == 4) {
+// Avataro keitimas
+elseif ($mid == 4) {
 		$sql = mysql_query1("SELECT `email` FROM `" . LENTELES_PRIESAGA . "users` WHERE `nick`=" . escape($_SESSION['username']) . " LIMIT 1");
 		if (isset($_GET['a']) && $_GET['a'] == 1)
 			@unlink('images/avatars/' . md5($sql['email']) . '.jpeg');
@@ -148,90 +148,80 @@ if (isset($mid) && isnum($mid)) {
 		$name = md5($sql['email']);
 		$gravatar = url("?id,{$_GET['id']};m,{$_GET['m']};a,1");
 		$avatar = <<<HTML
-<style type="text/css">
-
-
-
-</style>
-
-<script type="text/javascript" src="javascript/jquery/jquery.uploader.js"></script>
-
-<script  type="text/javascript">
-
-$(document).ready(function(){
-
-var button = $('#button1'), interval;
-new AjaxUpload(button,{
-action: 'upload.php',
-name: 'userfile',
-data: {
-email : '{$sql['email']}'
-
-},
-
-onSubmit : function(file, ext){
-$('#gravatar').hide();
-if (! (ext && /^(jpg|jpeg|png|gif|bmp)$/.test(ext))){
-alert('{$lang['admin']['download_badfile']}');
-return false;
-} else {
-button.html('<img src="images/icons/Loading.gif" />{$lang['user']['edit_uploading']}...');
-this.disable();
-}
-},
-onComplete: function(file, response){
-button.html('<img src="images/icons/picture__plus.png" alt="" class="middle"/>{$lang['user']['edit_upload']}');
-this.enable();
-
-$('#example1 .files').replaceWith('<div class="files"><img id="ikeltas_avataras" src="images/avatars/{$name}.jpeg?'+file+'" alt="" /></div>');
-}
-}); 
-});
-</script>
-<div align='center'>
-
-	<span id="example1" class="example">
-		
-	
-<a onclick="return false"><span id="button1"><img src="images/icons/picture__plus.png" alt="" class="middle"/> {$lang['user']['edit_upload']}</span></a> <br />
-<a class="btn" href="{$gravatar}"><span><img src="images/icons/picture__plus.png" alt="" class="middle"/> {$lang['user']['edit_gravatar']}</span></a>
-
-		<p>{$lang['user']['edit_avatar']}:</p>
-
-		<div class="files">{$avataras}</div>
-		</span>
-</div>
+        <script type="text/javascript" src="javascript/jquery/jquery.uploader.js"></script>
+        <script  type="text/javascript">
+           $(document).ready(function(){
+               var button = $('#button1'), interval;
+               new AjaxUpload(button,{
+                     action: 'upload.php',
+                     name: 'userfile',
+                     data: {
+                        email : '{$sql['email']}'
+                     },
+                     onSubmit : function(file, ext){
+                         $('#gravatar').hide();
+                         if (! (ext && /^(jpg|jpeg|png|gif|bmp)$/.test(ext))){
+                           alert('{$lang['admin']['download_badfile']}');
+                           return false;
+                         } else {
+                            button.html('<img src="images/icons/Loading.gif" />{$lang['user']['edit_uploading']}...');
+                            this.disable();
+                         }
+                     },
+                     onComplete: function(file, response){
+                        button.html('<img src="images/icons/picture__plus.png" alt="" class="middle"/>{$lang['user']['edit_upload']}');
+                        this.enable();
+                        $('#example1 .files').replaceWith('<div class="files"><img id="ikeltas_avataras" src="images/avatars/{$name}.jpeg?'+file+'" alt="" /></div>');
+                     }
+               });
+           });
+        </script>
+        <div align="center">
+         	<span id="example1" class="example">
+		        <a onclick="return false">
+		           <span id="button1">
+		              <img src="images/icons/picture__plus.png" alt="" class="middle"/> {$lang['user']['edit_upload']}
+		            </span>
+		        </a> <br />
+                <a class="btn" href="{$gravatar}">
+                   <span>
+                      <img src="images/icons/picture__plus.png" alt="" class="middle"/> {$lang['user']['edit_gravatar']}
+                   </span>
+                </a>
+             	<p>{$lang['user']['edit_avatar']}:</p>
+         		<div class="files">{$avataras}</div>
+	    	</span>
+        </div>
 HTML;
 		if (isset($_GET['a']) && $_GET['a'] == 1)
-			$avatar .= "<div align='center' id='gravatar'>
-{$lang['user']['edit_avatarcontent']} <b>" . input($sql['email']) . "</b> .</div>";
+			$avatar .= "<div align='center' id='gravatar'>{$lang['user']['edit_avatarcontent']} <b>" . input($sql['email']) . "</b>.</div>";
 
 		lentele($lang['user']['edit_avatar'], $avatar);
-	}
-	// Pagrindiniai nustatymai
-	elseif ($mid == 5) {
+}
+// Pagrindiniai nustatymai
+elseif ($mid == 5) {
 		$sql = mysql_query1("SELECT * FROM `" . LENTELES_PRIESAGA . "users` WHERE nick=" . escape($_SESSION['username']) . " LIMIT 1");
-    echo '<script src="javascript/jquery/jquery.maskedinput-1.2.2.js" type="text/javascript"></script><script type="text/javascript">jQuery(function($){
-   $("#date").mask("9999-99-99");
-   });
-</script>';
-		$forma = array(
+    echo '<script src="javascript/jquery/jquery.maskedinput-1.2.2.js" type="text/javascript"></script>
+          <script type="text/javascript">jQuery(function($){
+             $("#date").mask("9999-99-99");
+          });
+          </script>';
+		$form_array = array(
 			"Form" => array("action" => url("?id," . $conf['puslapiai'][basename(__file__)]['id'] . ";m," . $_GET['m']), "method" => "post", "name" => "parasas"),
 			"{$lang['user']['edit_name']}:" => array("type" => "text", "value" => input($sql['vardas']), "name" => "vardas", "class" => "input"),
 			"{$lang['user']['edit_secondname']}:" => array("type" => "text", "value" => input($sql['pavarde']), "name" => "pavarde", "class" => "input"),
 			"{$lang['user']['edit_dateOfbirth']}:" => array("type" => "text", "value" => input($sql['gim_data']), "extra" => "title='0000-00-00' size='10' maxlength='10' style='width:inherit'", "class" => "input", "name" => "gimimas", "id" => "date"),
-			//"\r " => array("type" => "select", "class" => "select", "value" => $day, "selected" => (isset($data[2]) ? $data[2] : 1), "name" => "diena"),
 			$lang['user']['edit_signature'] => array("type" => "textarea", "class" => "input", "value" => input($sql['parasas']), "name" => "parasas", "id" => "parasas"),
 			" " => array("type" => "string", "value" => bbk('parasas')),
 			" \r \n" => array("type" => "hidden", "name" => "action", "value" => "default_change"),
-			"" => array("type" => "submit", "value" => "{$lang['user']['edit_update']}")
+			"" => array("type" => "submit", "value" => $lang['user']['edit_update'])
 		);
 
-		$bla = new forma();
-		lentele($lang['user']['edit_mainsettings'], $bla->form($forma));
+		$form = new forma();
+		lentele($lang['user']['edit_mainsettings'], $form->form($form_array));
 		unset($data, $viso, $day, $month, $year);
-	}
 }
+
 unset($text);
 ?>
 <script type="text/javascript">
