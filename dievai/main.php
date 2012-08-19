69a43f72bbb9cd0cdce3f6ba605b523fd88b8bc5
zlotas <zlotas@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2012-08-19 21:41:14 +0000

    Atnaujinti: tiny_mce, nicEdit editoriai bei pridėtas naujas: ckeditor
    git-svn-id: http://subversion.assembla.com/svn/mightmedia/v1@839 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- dievai/main.php
+++ dievai/main.php
@@ -186,48 +186,39 @@ HTML;
       $dir = adresas();
       $return = <<<HTML
       <!-- Load TinyMCE -->
-<script type="text/javascript" src="{$dir}htmlarea/tiny_mce/jquery.tinymce.js"></script>
-<script src="{$dir}htmlarea/tiny_mce/tiny_mce_src.js" type="text/javascript"></script>
+<script src="{$dir}htmlarea/tiny_mce/tiny_mce.js" type="text/javascript"></script>
 <script type="text/javascript">
-	$(document).ready(function() {
-		$('textarea.tinymce').tinymce({
-			// Location of TinyMCE script
-			script_url : '{$dir}htmlarea/tiny_mce/tiny_mce.js',
+		tinyMCE.init({
+		// General options
+		plugins : "paste",
+		mode : "textareas",
+		theme : "advanced",
+		skin : "o2k7",
+		skin_variant : "silver",
+		plugins : "lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,inlinepopups,autosave",
 
-			// General options
-			theme : "advanced",
-			plugins : "pagebreak,style,layer,table,advimage,advlink,emotions,insertdatetime,preview,media,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,advlist",
+		// Theme options
+		theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
+		theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+		theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+		theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak,restoredraft",
+		theme_advanced_toolbar_location : "top",
+		theme_advanced_toolbar_align : "left",
+		theme_advanced_statusbar_location : "bottom",
+		theme_advanced_resizing : true,
 
-			// Theme options
-			theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
-			theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-			theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen,pagebreak",
-			
-			theme_advanced_toolbar_location : "top",
-			theme_advanced_toolbar_align : "left",
-			theme_advanced_statusbar_location : "bottom",
-			theme_advanced_resizing : true,
-			theme_advanced_resize_horizontal : false,
+		// Example content CSS (should be your site CSS)
+		content_css : "{$dir}htmlarea/tiny_mce/css/content.css",
 
-			apply_source_formatting : true,
-			theme_advanced_disable : "help,mce_ltr,mce_rtl",
-			entities : "38,amp,34,quot,60,lt,62,gt",
+		// Drop lists for link/image/media/template dialogs
+		template_external_list_url : "{$dir}htmlarea/tiny_mce/template_list.js",
+		external_link_list_url : "{$dir}htmlarea/tiny_mce/link_list.js",
+		external_image_list_url : "{$dir}htmlarea/tiny_mce/image_list.js",
+		media_external_list_url : "{$dir}htmlarea/tiny_mce/media_list.js",
 
-			// Example content CSS (should be your site CSS)
-			content_css : "{$dir}../stiliai/{$conf['Stilius']}/default.css",
 
-			// Drop lists for link/image/media/template dialogs
-			template_external_list_url : "lists/template_list.js",
-			external_link_list_url : "lists/link_list.js",
-			external_image_list_url : "lists/image_list.js",
-			media_external_list_url : "lists/media_list.js",
-      remove_script_host : false,
-      convert_urls : false
-
-			// Replace values for the template plugin
-			
-		});
 	});
+
 </script>
 <!-- /TinyMCE -->
 HTML;
@@ -235,15 +226,15 @@ HTML;
       foreach ($id as $key => $val) {
         $return .= <<<HTML
         <button onclick="window.open('htmlarea/markitup/utils/manager/index.php?id={$key}','mywindow','menubar=1,resizable=1,width=820,height=500');return false;
-" ><img src="../images/icons/pictures__plus.png" /> {$lang['admin']['insert_image']}</button>
-	<textarea id="{$key}" name="{$key}" style="min-height:200px; width: 100%;" class="tinymce">{$value[$key]}</textarea>
+" ><img src="../images/icons/pictures__plus.png" /> {$lang['admin']['insert_image']}</button><br />
+	<div style="width:750px;"><textarea id="{$key}" name="{$key}" style="width:750px;" class="tinymce">{$value[$key]}</textarea></div>
 HTML;
       }
     } else {
       $return .= <<<HTML
       <button onclick="window.open('htmlarea/markitup/utils/manager/index.php?id={$id}','mywindow','menubar=1,resizable=1,width=820,height=500');return false;
-"><img src="../images/icons/pictures__plus.png" /> {$lang['admin']['insert_image']}</button>
-<textarea id="{$id}" name="{$id}" style="min-height:200px; width: 100%;" class="tinymce">{$value}</textarea>
+"><img src="../images/icons/pictures__plus.png" /> {$lang['admin']['insert_image']}</button><br />
+<div style="width:750px;"><textarea id="{$id}" name="{$id}" style="width:750px;" class="tinymce">{$value}</textarea></div>
 HTML;
 
     }
@@ -259,27 +250,48 @@ HTML;
 
 			$return .= <<< HTML
 <script type="text/javascript">
-$(document).ready(function(){
-	new nicEditor({fullPanel : true, iconsPath : '{$dir}htmlarea/nicedit/nicEditorIcons.gif'}).panelInstance('{$key}');
-	
+bkLib.onDomLoaded(function() {
+	new nicEditor({fullPanel : true, iconsPath : '{$dir}htmlarea/nicedit/nicEditorIcons.gif', width: '100%'}).panelInstance('{$key}');
 });
 </script>
-<textarea id="{$key}" cols="40" name="{$key}">{$value[$key]}</textarea>
+<textarea id="{$key}" name="{$key}" style="width: 750px;">{$value[$key]}</textarea>
 HTML;
 		}
 	} else {
 		$return .= <<< HTML
 <script type="text/javascript">
-$(document).ready(function(){
+bkLib.onDomLoaded(function() {
 	new nicEditor({fullPanel : true, iconsPath : '{$dir}htmlarea/nicedit/nicEditorIcons.gif', width: '100%'}).panelInstance('{$id}');
-	
 });
 </script>
-<textarea id="{$id}" cols="40" name="{$id}">{$value}</textarea>
+
+<textarea id="{$id}" name="{$id}" style="width: 750px;">{$value}</textarea>
+HTML;
+
+	}
+	
+	}elseif($conf['Editor'] == 'ckeditor'){
+        $dir = adresas();
+
+    	$return = <<<HTML
+<script type="text/javascript" src="{$dir}htmlarea/ckeditor/ckeditor.js"></script>
+HTML;
+
+	if (is_array($id)) {
+		foreach ($id as $key => $val) {
+
+			$return .= <<< HTML
+<textarea id="{$key}" name="{$key}" class="ckeditor">{$value[$key]}</textarea>
+HTML;
+		}
+	} else {
+		$return .= <<< HTML
+<textarea id="{$id}" name="{$id}" class="ckeditor">{$value}</textarea>
 HTML;
 
 	}
 	}
+
 	return $return;
 }
 ?>
