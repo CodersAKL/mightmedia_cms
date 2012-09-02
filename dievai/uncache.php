562101efe5e1f01b82379a70616efd3bad41d8e9
zlotas <zlotas@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2012-09-02 20:48:49 +0000

    atnaujiname dievai katalogą
    git-svn-id: http://subversion.assembla.com/svn/mightmedia/v1@866 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- dievai/uncache.php
+++
@@ -1,7 +0,0 @@
-<?php
-  foreach (glob(ROOT."sandeliukas/*.php") as $filename) {
-    unlink($filename);
-  }
-  //echo 'TODO: PADARYT kad valytų iš tikro, reik folderio tuštinimo funkcijos.';
-  msg($lang['system']['done'], $lang['admin']['uncached']);
-?>
