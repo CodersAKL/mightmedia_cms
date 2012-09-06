263f8851f71e0424d2e0dfa56d67ad2a0644a830
zlotas <zlotas@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2012-09-06 22:45:56 +0000

    blokai lygiavimas :D
    git-svn-id: http://subversion.assembla.com/svn/mightmedia/v1@952 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- blokai/reksniu_deze.php
+++ blokai/reksniu_deze.php
@@ -10,28 +10,28 @@
  * @$Date: 2012-08-17 10:15:31 +0300 (Pn, 17 Rgp 2012) $
  **/
 
-if(isset($_POST['chat_box']) && !empty($_POST['chat_box']) && !empty($_POST['chat_msg'])) {
-	if(!isset($_COOKIE['komentatorius']) || (isset($_POST['name']) && $_POST['name'] != $_COOKIE['komentatorius'])) {
-		setcookie("komentatorius",$_POST['name'],time() + 60 * 60 * 24 * 30);
+if ( isset( $_POST['chat_box'] ) && !empty( $_POST['chat_box'] ) && !empty( $_POST['chat_msg'] ) ) {
+	if ( !isset( $_COOKIE['komentatorius'] ) || ( isset( $_POST['name'] ) && $_POST['name'] != $_COOKIE['komentatorius'] ) ) {
+		setcookie( "komentatorius", $_POST['name'], time() + 60 * 60 * 24 * 30 );
 	}
-	$msg		= htmlspecialchars($_POST['chat_msg']);
-	$nick_id	= (isset($_SESSION['id']) ? $_SESSION['id'] : 0);
-	$nick		= (isset($_SESSION['username']) ? $_SESSION['username'] : (!empty($_POST['name']) ? $_POST['name'] : $lang['system']['guest']));
+	$msg     = htmlspecialchars( $_POST['chat_msg'] );
+	$nick_id = ( isset( $_SESSION['id'] ) ? $_SESSION['id'] : 0 );
+	$nick    = ( isset( $_SESSION['username'] ) ? $_SESSION['username'] : ( !empty( $_POST['name'] ) ? $_POST['name'] : $lang['system']['guest'] ) );
 
-	mysql_query1("INSERT INTO `".LENTELES_PRIESAGA."chat_box`
+	mysql_query1( "INSERT INTO `" . LENTELES_PRIESAGA . "chat_box`
 		(`nikas`, `msg`, `time`, `niko_id`)
-		VALUES (".escape($nick).", ".escape($msg).", NOW(), ".escape($nick_id).");"
+		VALUES (" . escape( $nick ) . ", " . escape( $msg ) . ", NOW(), " . escape( $nick_id ) . ");"
 	);
-	mysql_query1("DELETE FROM `".LENTELES_PRIESAGA."chat_box` WHERE time < (NOW() - INTERVAL 31 DAY)");
-	redirect($_SERVER['HTTP_REFERER'],"header");
+	mysql_query1( "DELETE FROM `" . LENTELES_PRIESAGA . "chat_box` WHERE time < (NOW() - INTERVAL 31 DAY)" );
+	redirect( $_SERVER['HTTP_REFERER'], "header" );
 }
-$extra = '';
-$vardas = (isset($_COOKIE['komentatorius']) ? $_COOKIE['komentatorius'] : $lang['system']['guest']);
-$sveciams = (isset($conf['kmomentarai_sveciams']) && $conf['kmomentarai_sveciams'] == 1);
-if((isset($_SESSION['username']) && !empty($_SESSION['username'])) || $sveciams) {
+$extra    = '';
+$vardas   = ( isset( $_COOKIE['komentatorius'] ) ? $_COOKIE['komentatorius'] : $lang['system']['guest'] );
+$sveciams = ( isset( $conf['kmomentarai_sveciams'] ) && $conf['kmomentarai_sveciams'] == 1 );
+if ( ( isset( $_SESSION['username'] ) && !empty( $_SESSION['username'] ) ) || $sveciams ) {
 	$chat_box = "<form name=\"chat_box\" action=\"\" method=\"post\">
-	               ".($sveciams && !isset($_SESSION['username']) ?
-                   '<input type="text" name="name" class="submit" value="'.$vardas.'"/>' : '')."
+	               " . ( $sveciams && !isset( $_SESSION['username'] ) ?
+		'<input type="text" name="name" class="submit" value="' . $vardas . '"/>' : '' ) . "
                    <textarea name=\"chat_msg\" rows=\"3\" cols=\"10\" class=\"input\" style=\"margin-bottom:5px;\"></textarea>
                    <input type=\"submit\" name=\"chat_box\" class=\"submit\" value=\"{$lang['sb']['send']}\" />
                </form>";
@@ -40,32 +40,32 @@ if((isset($_SESSION['username']) && !empty($_SESSION['username'])) || $sveciams)
 }
 $chat_box .= "<div class='line'></div>";
 $extras = "";
- //usklausa irasam, priklausomai nuo to, ar sveciams galima rasyt.
-if(isset($conf['kmomentarai_sveciams']) && $conf['kmomentarai_sveciams'] == 1) {
-	$chat = mysql_query1("SELECT * FROM `".LENTELES_PRIESAGA."chat_box` ORDER BY `time` DESC LIMIT ".escape((int) $conf['Chat_limit']));
+//usklausa irasam, priklausomai nuo to, ar sveciams galima rasyt.
+if ( isset( $conf['kmomentarai_sveciams'] ) && $conf['kmomentarai_sveciams'] == 1 ) {
+	$chat = mysql_query1( "SELECT * FROM `" . LENTELES_PRIESAGA . "chat_box` ORDER BY `time` DESC LIMIT " . escape( (int)$conf['Chat_limit'] ) );
 } else {
-	$chat = mysql_query1("SELECT 
-		`".LENTELES_PRIESAGA."chat_box`.*,
-		`".LENTELES_PRIESAGA."users`.`nick`,
-		`".LENTELES_PRIESAGA."users`.`levelis`
-		FROM `".LENTELES_PRIESAGA."chat_box`
-		Inner Join `".LENTELES_PRIESAGA."users`
-		ON `".LENTELES_PRIESAGA."chat_box`.`niko_id` = `".LENTELES_PRIESAGA."users`.`id`
-		ORDER BY `time` DESC LIMIT ".escape((int) $conf['Chat_limit'])
+	$chat = mysql_query1( "SELECT
+		`" . LENTELES_PRIESAGA . "chat_box`.*,
+		`" . LENTELES_PRIESAGA . "users`.`nick`,
+		`" . LENTELES_PRIESAGA . "users`.`levelis`
+		FROM `" . LENTELES_PRIESAGA . "chat_box`
+		Inner Join `" . LENTELES_PRIESAGA . "users`
+		ON `" . LENTELES_PRIESAGA . "chat_box`.`niko_id` = `" . LENTELES_PRIESAGA . "users`.`id`
+		ORDER BY `time` DESC LIMIT " . escape( (int)$conf['Chat_limit'] )
 	);
 }
 $i = 0;
 //irasu sarasas
-if(sizeof($chat) > 0) {
-	foreach($chat as $row) {
+if ( sizeof( $chat ) > 0 ) {
+	foreach ( $chat as $row ) {
 		$i++;
 		$tr = $i % 2 ? '2' : '';
-		if(ar_admin('com') && puslapis('deze.php')) {
-			$extras = "<a style=\"float: right;\" title=\"{$lang['admin']['delete']}\" href=\"" . url("?id,{$conf['puslapiai']['deze.php']['id']};d,{$row['id']}") . "\" onclick=\"return confirm('{$lang['system']['delete_confirm']}')\"><img height=\"12\" src=\"images/icons/cross.png\" alt=\"[d]\" class=\"middle\" border=\"0\" /></a> 
-			<a style=\"float: right;\" title=\"{$lang['admin']['edit']}\" href=\"" . url("?id,{$conf['puslapiai']['deze.php']['id']};r,{$row['id']}") . "\"><img height=\"12\" src=\"images/icons/pencil.png\" alt=\"[r]\" class=\"middle\" border=\"0\" /></a>";
+		if ( ar_admin( 'com' ) && puslapis( 'deze.php' ) ) {
+			$extras = "<a style=\"float: right;\" title=\"{$lang['admin']['delete']}\" href=\"" . url( "?id,{$conf['puslapiai']['deze.php']['id']};d,{$row['id']}" ) . "\" onclick=\"return confirm('{$lang['system']['delete_confirm']}')\"><img height=\"12\" src=\"images/icons/cross.png\" alt=\"[d]\" class=\"middle\" border=\"0\" /></a>
+			<a style=\"float: right;\" title=\"{$lang['admin']['edit']}\" href=\"" . url( "?id,{$conf['puslapiai']['deze.php']['id']};r,{$row['id']}" ) . "\"><img height=\"12\" src=\"images/icons/pencil.png\" alt=\"[r]\" class=\"middle\" border=\"0\" /></a>";
 		}
-		$chat_box .= "<div class=\"tr{$tr}\"><b>" . user($row['nikas'],$row['niko_id']) . "</b><font style='font-size:9px;'><em>(" . $row['time'] . ")</em></font>{$extras}
-		<br />" . smile(bbchat(wrap($row['msg'],18))) . "<br />
+		$chat_box .= "<div class=\"tr{$tr}\"><b>" . user( $row['nikas'], $row['niko_id'] ) . "</b><font style='font-size:9px;'><em>(" . $row['time'] . ")</em></font>{$extras}
+		<br />" . smile( bbchat( wrap( $row['msg'], 18 ) ) ) . "<br />
 		
 		</div>";
 	}
@@ -73,8 +73,8 @@ if(sizeof($chat) > 0) {
 	$chat_box .= "";
 }
 //jei archyvo psl ijungtas, rodom nuoroda
-if(puslapis('deze.php')) {
-	$chat_box .= "<a href=\"" . url("?id,{$conf['puslapiai']['deze.php']['id']}") . "\" >{$lang['sb']['archive']}</a>";
+if ( puslapis( 'deze.php' ) ) {
+	$chat_box .= "<a href=\"" . url( "?id,{$conf['puslapiai']['deze.php']['id']}" ) . "\" >{$lang['sb']['archive']}</a>";
 }
 
 
--- blokai/vartotojas.php
+++ blokai/vartotojas.php
@@ -11,7 +11,7 @@
  **/
 
 ####################### Atvaizduojam panele ###########################
-if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
+if ( !isset( $_SESSION['username'] ) || empty( $_SESSION['username'] ) ) {
 	$text = <<< HTML
 	<center>
 		<form id="user_reg" name="user_reg" method="post" action="">
@@ -24,39 +24,39 @@ if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
 		</form>
 	</center>
 HTML;
-		if (puslapis('reg.php')) {
-			$text .= "<a href=\"".url("?id," . $conf['puslapiai']['reg.php']['id'] ). "\">{$lang['user']['registration']}</a> ";
-		}
-	if (puslapis('slaptazodzio_priminimas.php')) {
-		$text .= "<a href=\"".url("?id," . $conf['puslapiai']['slaptazodzio_priminimas.php']['id'] ). "\">{$lang['user']['pass_forget']}</a>";
+	if ( puslapis( 'reg.php' ) ) {
+		$text .= "<a href=\"" . url( "?id," . $conf['puslapiai']['reg.php']['id'] ) . "\">{$lang['user']['registration']}</a> ";
+	}
+	if ( puslapis( 'slaptazodzio_priminimas.php' ) ) {
+		$text .= "<a href=\"" . url( "?id," . $conf['puslapiai']['slaptazodzio_priminimas.php']['id'] ) . "\">{$lang['user']['pass_forget']}</a>";
 		$title = $lang['user']['for_members'];
 	}
 } else {
 	//$user = $_SESSION['username'];
 	$text = "<ul>";
 	//profilio nuoroda
-	if (isset($conf['puslapiai']['edit_user.php']['id'])) {
-		$text .= "<li><a href=\"".url("?id," . $conf['puslapiai']['view_user.php']['id']) . "/".$_SESSION['username']."\"><img src=\"images/icons/user-white.png\" alt=\"@\" style=\"vertical-align: middle;\" border=\"0\" /> {$lang['user']['profile']}</a></li>";
+	if ( isset( $conf['puslapiai']['edit_user.php']['id'] ) ) {
+		$text .= "<li><a href=\"" . url( "?id," . $conf['puslapiai']['view_user.php']['id'] ) . "/" . $_SESSION['username'] . "\"><img src=\"images/icons/user-white.png\" alt=\"@\" style=\"vertical-align: middle;\" border=\"0\" /> {$lang['user']['profile']}</a></li>";
 	}
 	//profilio redagavimo nuoroda
-	if (isset($conf['puslapiai']['edit_user.php']['id'])) {
-		$text .= "<li><a href=\"".url("?id," . $conf['puslapiai']['edit_user.php']['id']) . "\"><img src=\"images/icons/user_edit.png\" alt=\"@\" style=\"vertical-align: middle;\" border=\"0\" /> {$lang['user']['edit_profile']}</a></li>";
+	if ( isset( $conf['puslapiai']['edit_user.php']['id'] ) ) {
+		$text .= "<li><a href=\"" . url( "?id," . $conf['puslapiai']['edit_user.php']['id'] ) . "\"><img src=\"images/icons/user_edit.png\" alt=\"@\" style=\"vertical-align: middle;\" border=\"0\" /> {$lang['user']['edit_profile']}</a></li>";
 	}
 	//Pm nuoroda
-	if (puslapis('pm.php')) {
-		$pm = kiek('private_msg', "WHERE `to`=" . escape($_SESSION['username']) . " AND `read`='NO'", 'total');
+	if ( puslapis( 'pm.php' ) ) {
+		$pm = kiek( 'private_msg', "WHERE `to`=" . escape( $_SESSION['username'] ) . " AND `read`='NO'", 'total' );
 
-		if ($pm != 0) {
+		if ( $pm != 0 ) {
 			$img = "<img src='images/icons/email_error.gif' alt='new' border='0' style=\"vertical-align: middle;\"/>";
 		} else {
 			$img = "<img src='images/icons/email.png' alt='@' border='0' style=\"vertical-align: middle;\" />";
 		}
-		$text .= "<li><a href=\"".url("?id," . $conf['puslapiai']['pm.php']['id'] . ";a,1")."\">$img {$lang['user']['messages']} ({$pm})</a></li>";
+		$text .= "<li><a href=\"" . url( "?id," . $conf['puslapiai']['pm.php']['id'] . ";a,1" ) . "\">$img {$lang['user']['messages']} ({$pm})</a></li>";
 	}
-  //moderatoriaus puslapio nuoroda
-	if (puslapis('moderatorius.php') && !empty($_SESSION['mod'])) {
+	//moderatoriaus puslapio nuoroda
+	if ( puslapis( 'moderatorius.php' ) && !empty( $_SESSION['mod'] ) ) {
 		$text .= "
-			<li><a href=\"".url("?id,{$conf['puslapiai']['moderatorius.php']['id']}")."\"><img src=\"images/icons/book__pencil.png\" alt=\"@\" style=\"vertical-align: middle;\" border=\"0\" /> {$lang['system']['mod']}</a></li>";
+			<li><a href=\"" . url( "?id,{$conf['puslapiai']['moderatorius.php']['id']}" ) . "\"><img src=\"images/icons/book__pencil.png\" alt=\"@\" style=\"vertical-align: middle;\" border=\"0\" /> {$lang['system']['mod']}</a></li>";
 	}
 
 	//atsijungimo nuoroda
@@ -64,6 +64,6 @@ HTML;
 			<li><a href=\"{$lang['user']['logout']}\"><img src=\"images/icons/key_go.png\" alt=\"@\" style=\"vertical-align: middle;\" border=\"0\" /> {$lang['user']['logout']}</a></li>
 		</ul>
 	";
-	$title = sprintf($lang['user']['hello'], $_SESSION['username']);
+	$title = sprintf( $lang['user']['hello'], $_SESSION['username'] );
 }
 ?>
