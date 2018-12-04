615264bfa0fc653773246fb5dc26ea139ad94586
Aivaras Čenkus <aivaras.cenkus@gmail.com>
2018-12-04 18:32:50 +0200

    missed install dir;

---
+++ install/steps/3/controller.php
@@ -0,0 +1,17 @@
+<?php
+// Sarašas failų kurių teisės turi suteikti svetainei įrašymo galimybę
+$chmodFiles = [
+	'install/index.php',
+	'.htaccess',
+	'siuntiniai/failai',
+	'siuntiniai/images',
+	'siuntiniai/media',
+	'sandeliukas',
+	'puslapiai',
+	'blokai',
+	'images/avatars',
+	'images/nuorodu',
+	'images/galerija',
+	'images/galerija/originalai',
+	'images/galerija/mini'
+];
