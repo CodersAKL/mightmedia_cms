7488820a225b16e836f7c25889ce2b473087c6e1
jonasburneika <jonas.burneika@gmail.com>
2018-12-23 19:46:01 +0200

    ROOT defined with / at the end.

--- core/functions/functions.core.php
+++ core/functions/functions.core.php
@@ -2,7 +2,7 @@
 
 function config($file, $key = null)
 {
-    $config = include ROOT . '/core/configs/config.' . $file . '.php';
+    $config = include ROOT . 'core/configs/config.' . $file . '.php';
 
     return ! empty($key) ? $config[$key] : $config;
 }
@@ -243,7 +243,7 @@ function kodas() {
 	var \$human = document.createElement('img');
 	\$human.setAttribute('src','core/inc/inc.human.php');
 	\$human.setAttribute('style','cursor: pointer');
-	\$human.setAttribute('title','{$lang['system']['refresh']}');
+	\$human.setAttribute('title','{getLangText('system', 'refresh')}');
 	\$human.setAttribute('onclick','this.src="core/inc/inc.human.php?"+Math.random();');
 	\$human.setAttribute('alt','code');
 	\$human.setAttribute('id','captcha');
