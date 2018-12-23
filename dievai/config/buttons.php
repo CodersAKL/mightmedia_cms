412616486c7cdb729fdd5dd83e36137fba863ee3
jonasburneika <jonas.burneika@gmail.com>
2018-12-23 14:00:11 +0200

    first test for inline translation function

--- dievai/config/buttons.php
+++ dievai/config/buttons.php
@@ -4,115 +4,115 @@ $buttons = [
     'news'      => [
         [
             'url' 	=> url( "?id,999;a,news;v,6" ),
-            'value'	=> $lang['admin']['news_unpublished'],
+            'value'	=> getLangText('admin','news_unpublished'),
             'icon'	=> adminIcon('news', 'unpublished')
         ],
         [
             'url' 	=> url( "?id,999;a,news;v,1" ),
-            'value'	=> $lang['admin']['news_create'],
+            'value'	=> getLangText('admin','news_create'),
             'icon'	=> adminIcon('news', 'create')
         ],
         [
             'url' 	=> url( "?id,999;a,news;v,4" ),
-            'value'	=> $lang['admin']['news_edit'],
+            'value'	=> getLangText('admin','news_edit'),
             'icon'	=> adminIcon('news', 'edit')
         ],
         [
             'url' 	=> url( "?id,999;a,news;v,2" ),
-            'value'	=> $lang['system']['createcategory'],
+            'value'	=> getLangText('system','createcategory'),
             'icon'	=> adminIcon('news', 'create_category')
         ],
         [
             'url' 	=> url( "?id,999;a,news;v,3" ),
-            'value'	=> $lang['system']['editcategory'],
+            'value'	=> getLangText('system','editcategory'),
             'icon'	=> adminIcon('news', 'create_category')
         ]
     ],
     'blocks'    => [
         [
             'url' 	=> url( "?id,999;a,blocks;" ),
-            'value'	=> $lang['admin']['blocks'],
+            'value'	=> getLangText('admin','blocks'),
             'icon'	=> adminIcon('blocks', 'select')
         ],
         [
             'url' 	=> url( "?id,999;a,blocks;n,1" ),
-            'value'	=> $lang['admin']['panel_select'],
+            'value'	=> getLangText('admin','panel_select'),
             'icon'	=> adminIcon('blocks', 'select')
         ],
         [
             'url' 	=> url( "?id,999;a,blocks;n,2" ),
-            'value'	=> $lang['admin']['panel_create'],
+            'value'	=> getLangText('admin','panel_create'),
             'icon'	=> adminIcon('blocks', 'create')
         ]
     ],
     'users'     => [
         [
             'url' 	=> url( "?id,999;a,users;v,1" ),
-            'value'	=> $lang['admin']['user_list'],
+            'value'	=> getLangText('admin','user_list'),
             'icon'	=> adminIcon('users', 'list')
         ],
         [
             'url' 	=> url( "?id,999;a,users;v,4" ),
-            'value'	=> $lang['admin']['user_find'],
+            'value'	=> getLangText('admin','user_find'),
             'icon'	=> adminIcon('users', 'find')
         ],
         [
             'url' 	=> url( "?id,999;a,users;v,2" ),
-            'value'	=> $lang['system']['createcategory'],
+            'value'	=> getLangText('system','createcategory'),
             'icon'	=> adminIcon('users', 'create_category')
         ],
         [
             'url' 	=> url( "?id,999;a,users;v,3" ),
-            'value'	=> $lang['system']['editcategory'],
+            'value'	=> getLangText('system','editcategory'),
             'icon'	=> adminIcon('users', 'create_category')
         ]
     ],
     'pages'     => [
         [
             'url' 	=> url( "?id,999;a,pages;" ),
-            'value'	=> $lang['admin']['meniu'],
+            'value'	=> getLangText('admin','meniu'),
             'icon'	=> adminIcon('pages', 'select')
         ],
         [
             'url' 	=> url( "?id,999;a,pages;n,1" ),
-            'value'	=> $lang['admin']['page_select'],
+            'value'	=> getLangText('admin','page_select'),
             'icon'	=> adminIcon('pages', 'select')
         ],
         [
             'url' 	=> url( "?id,999;a,pages;n,2" ),
-            'value'	=> $lang['admin']['page_create'],
+            'value'	=> getLangText('admin','page_create'),
             'icon'	=> adminIcon('pages', 'create')
         ],
         [
             'url' 	=> url( "?id,999;a,pages;n,3" ),
-            'value'	=> $lang['admin']['page_tree'],
+            'value'	=> getLangText('admin','page_tree'),
             'icon'	=> adminIcon('pages', 'edit')
         ]
     ],
     'configuration' => [
         [
             'url' 	=> url("?id,999;a,configuration;c,main"),
-            'value'	=> $lang['admin']['configuration_main'],
+            'value'	=> getLangText('admin','configuration_main'),
             'icon'	=> adminIcon('configuration', 'main')
         ],
         [
             'url' 	=> url("?id,999;a,configuration;c,seo"),
-            'value'	=> $lang['admin']['configuration_seo'],
+            'value'	=> getLangText('admin','configuration_seo'),
             'icon'	=> adminIcon('configuration', 'seo')
         ],
         [
             'url' 	=> url("?id,999;a,configuration;c,maintenance"),
-            'value'	=> $lang['admin']['configuration_maintenance'],
+            'value'	=> getLangText('admin','configuration_maintenance'),
             'icon'	=> adminIcon('configuration', 'maintenance')
         ],
         [
             'url' 	=> url("?id,999;a,configuration;c,extensions"),
-            'value'	=> $lang['admin']['configuration_extensions'],
+            'value'	=> getLangText('admin','configuration_extensions'),
             'icon'	=> adminIcon('configuration', 'extensions')
         ],
         [
             'url' 	=> url("?id,999;a,configuration;c,translation"),
-            'value'	=> $lang['admin']['configuration_translations'],
+            'value'	=> getLangText('admin','configuration_translations'),
             'icon'	=> adminIcon('configuration', 'translation')
         ]
     ]
